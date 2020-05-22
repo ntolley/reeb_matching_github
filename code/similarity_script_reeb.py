df9f3dee577089a447ed31772014083b0e6ea5c8
Nick Tolley <nic.m.tolley@gmail.com>
2020-05-22 11:18:28 -0400

    Save workspace for script run

--- code/similarity_script_reeb.py
+++ code/similarity_script_reeb.py
@@ -30,7 +30,7 @@ file_list_tree = reeb_matching.get_skeleton_names(s_dir)
 resolution_list = [16,8,4,2]
 similarity_matrix_tree, MPAIR_list = reeb_matching.tree_sim_matrix(file_list_tree, resolution_list, data_dir, prefix)
 
-tree_sim_save = save_dir + '/similarity_matrix_tree.csv'
-tree_file_list_save = save_dir + '/file_list_tree.txt'
+tree_sim_save = save_dir + '/similarity_matrix_reeb.csv'
+tree_file_list_save = save_dir + '/file_list_reeb.txt'
 np.savetxt(tree_sim_save, similarity_matrix_tree, delimiter=',')
-np.savetxt(tree_file_list_save, np.array(file_list_tree), delimiter=',')
+np.savetxt(tree_file_list_save, np.array(file_list_tree), delimiter=',',fmt="%s")
