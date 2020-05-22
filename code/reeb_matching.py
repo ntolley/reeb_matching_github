Fix reeb matching by scaling node heights to (-1,1)

code/reeb_matching.py
@@ -21,6 +21,7 @@ import paramrw
 import itertools
 from scipy.spatial.distance import cdist
 from scipy.optimize import linear_sum_assignment
+from sklearn.preprocessing import MinMaxScaler
 sns.set()
     
 
@@ -33,8 +34,11 @@ def load_tree(data_dir,prefix,file_name):
     csd_nodes_df = pd.read_csv(s_dir + '/' + file_name + '_nodes.csv', sep=',')
     csd_connectivity_df = pd.read_csv(s_dir + '/' + file_name + '_arcs.csv', sep=',')
 
+    scaler = MinMaxScaler(feature_range=(-1,1))
 
     node_points = np.array(csd_nodes_df)
+    node_points[:2] = scaler.fit_transform(node_points[:2])
+
     node_connectivity = np.array(csd_connectivity_df)
     node_connectivity = node_connectivity.astype(int) - 1
 
@@ -60,7 +64,7 @@ def tree_sim_matrix(file_list, resolution_list, data_dir, prefix):
                 MPAIR_list.append(MPAIR)
 
                 print(tree_row,tree_col,'tree_sim = ', similarity)
-                print(MPAIR)
+                # print(MPAIR)
 
     return similarity_matrix, MPAIR_list
 

code/similarity_script_reeb.py
@@ -18,20 +18,11 @@ prefix = 'gbarEvPyrAmpa_sweep'
 
 #Setup file paths
 data_dir = os.path.abspath('lfp_reeb_github/data')
-os.mkdir('data/'+ prefix + '/similarity_matrices')
+
+if not os.path.isdir('data/'+ prefix + '/similarity_matrices'):
+    os.mkdir('data/'+ prefix + '/similarity_matrices')
+    
 save_dir = os.path.abspath('data/' + prefix + '/similarity_matrices')
-
-#Calculate similarity matrix for 2D wasserstein
-p_dir = data_dir + '/' + prefix + '/' + 'points/'
-file_list_csd = os.listdir(p_dir) 
-similarity_matrix_wasserstein = reeb_matching.wasserstein_sim_matrix(file_list_csd, data_dir,prefix)
-
-wasserstein_file_list_save = save_dir + '/file_list_wasserstein.csv'
-wasserstein_sim_save = save_dir + '/similarity_matrix_wasserstein.csv'
-np.savetxt('similarity_matrix_wasserstein.csv', similarity_matrix_wasserstein, delimiter=',')
-np.savetxt(wasserstein_sim_save, similarity_matrix_wasserstein, delimiter=',')
-np.savetxt(wasserstein_file_list_save, np.array(file_list_csd), delimiter=',')
-
 
 #Calculate similarity matrix for reeb_matching
 s_dir = data_dir + '/' + prefix + '/' + 'skeleton/'
@@ -42,6 +33,4 @@ similarity_matrix_tree, MPAIR_list = reeb_matching.tree_sim_matrix(file_list_tre
 tree_sim_save = save_dir + '/similarity_matrix_tree.csv'
 tree_file_list_save = save_dir + '/file_list_tree.csv'
 np.savetxt(tree_sim_save, similarity_matrix_tree, delimiter=',')
-np.savetxt(tree_file_list_save, file_list_tree, delimiter=',')
-
-
+np.savetxt(tree_file_list_save, np.array(file_list_tree), delimiter=',')